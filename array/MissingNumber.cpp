53a8057fa4804bd7524cd12b9fa0e1b3a5e6d68e
Sachan55 <33194654+Sachan55@users.noreply.github.com>
2020-05-30 15:54:19 +0530

    Create MissingNumber.cpp

---
+++ array/MissingNumber.cpp
@@ -0,0 +1,24 @@
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 1 ; i <=n ; i++)
+        {
+            ans = ans^i;
+        }
+        for(int j = 0 ; j < nums.size() ; j++)
+        {
+            ans = ans^nums[j];
+        }
+        return ans;
+    }
+};
